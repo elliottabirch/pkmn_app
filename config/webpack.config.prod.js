bfb274b0b01e63e417ac2af54fb08195dee6c145
elliottabirch <elliottabirch@gmail.com>
2017-04-08 03:24:18 -0700

    added server script to package.json

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,26 +1,26 @@
-let autoprefixer = require('autoprefixer');
-let webpack = require('webpack');
-let HtmlWebpackPlugin = require('html-webpack-plugin');
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
-let ManifestPlugin = require('webpack-manifest-plugin');
-let InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
-let url = require('url');
-let paths = require('./paths');
-let getClientEnvironment = require('./env');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
+const url = require('url');
+const paths = require('./paths');
+const getClientEnvironment = require('./env');
 
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
-let publicPath = paths.servedPath;
+const publicPath = paths.servedPath;
 // Some apps do not use client-side routing with pushState.
 // For these, "homepage" can be set to "." to enable relative asset paths.
-let shouldUseRelativeAssetPaths = publicPath === './';
+const shouldUseRelativeAssetPaths = publicPath === './';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
-let publicUrl = publicPath.slice(0, -1);
+const publicUrl = publicPath.slice(0, -1);
 // Get environment variables to inject into our app.
-let env = getClientEnvironment(publicUrl);
+const env = getClientEnvironment(publicUrl);
 
 // Assert this just to be safe.
 // Development builds of React are slow and not intended for production.
@@ -141,8 +141,7 @@ module.exports = {
         loader: ExtractTextPlugin.extract(
           'style',
           'css?importLoaders=1!postcss',
-          extractTextPluginOptions,
-        ),
+          extractTextPluginOptions),
         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
       },
       // JSON is not enabled by default in Webpack but both Node and Browserify
@@ -165,7 +164,7 @@ module.exports = {
   },
 
   // We use PostCSS for autoprefixing only.
-  postcss () {
+  postcss() {
     return [
       autoprefixer({
         browsers: [
