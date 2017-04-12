b4c7d9472ca1891ecad5dc50c78fc90ddafb6e29
elliottabirch <elliottabirch@gmail.com>
2017-04-11 20:41:56 -0700

    test db url

--- db/db.js
+++ db/db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const DBUrl = process.env.MONGODB_URI || 'mongodb://localhost/pkmnnames';
 
+console.log(DBUrl);
+
 mongoose.connect(DBUrl);
 const db = mongoose.connection;
 
